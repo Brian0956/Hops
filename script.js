75c4ed47ff6fa68d2230a6409f5690913e405e9b
Brian0956 <mielbrianjabian2004@gmail.com>
2026-05-02 20:31:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+window.addEventListener("click", () => {
+  const music = document.getElementById("bg-music");
+  music.play();
+});
+
+const music = document.getElementById("bg-music");
+
+window.addEventListener("click", () => {
+  music.muted = false;
+  music.volume = 0.3; // soft volume
+  music.play();
+});
